2bb519d4eb7da6a37c34999c1389c2bfca188ce8
Katherine Otten Kim <kkatherine317@gmail.com>
2022-07-26 20:22:43 -0700

    add descriptiona attribute to expense model

--- app/models/expense.py
+++ app/models/expense.py
@@ -4,6 +4,7 @@ class Expense(db.Model):
     expense_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     amount = db.Column(db.Numeric(15,2))
     category = db.Column(db.String)
+    description = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     user = db.relationship("User", backpopulate='expenses')
 
--- app/routes/expense_routes.py
+++ app/routes/expense_routes.py
@@ -4,30 +4,30 @@ from datetime import datetime
 from app.models.user import User
 from app.models.expense import Expense
 
-expense_bp = Blueprint('expense_bp', __name__, url_prefix='/expense')
+# expense_bp = Blueprint('expense_bp', __name__, url_prefix='/expense')
 
-@expense_bp.route("/<user_id>/<category>", methods=['GET'])
-def get_all_expenses():
+# @expense_bp.route("/<user_id>/", methods=['GET'])
+# def get_all_expenses():
     
 
-    selected_user = User.query.filter(User.email == user_email)
+#     selected_user = User.query.filter(User.email == user_email)
 
-    if not selected_user:
-        abort(make_response({'msg': 'user not found'}, 404))
+#     if not selected_user:
+#         abort(make_response({'msg': 'user not found'}, 404))
     
-    return selected_user
+#     return selected_user
     
 
-@user_bp.route("", methods=['POST'])
-def new_user():
-    request_body = request.get_json()
+# @user_bp.route("", methods=['POST'])
+# def new_user():
+#     request_body = request.get_json()
     
-    new_user = User(id_token=request_body['id_token'], email=request_body['email'], name=request_body['name'])
+#     new_user = User(id_token=request_body['id_token'], email=request_body['email'], name=request_body['name'])
 
-    db.session.add(new_user)
-    db.session.commit()
-    return {
-        'id': new_user.user_id,
-        'email': new_user.email,
-        'msg': f'{new_user.name} has successfully created an account'
-    }, 201
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return {
+#         'id': new_user.user_id,
+#         'email': new_user.email,
+#         'msg': f'{new_user.name} has successfully created an account'
+#     }, 201
