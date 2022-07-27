c9a34a1299315d4eef1acfaf1055740173a51734
Katherine Otten Kim <kkatherine317@gmail.com>
2022-07-27 13:24:08 -0700

    complete user routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -31,36 +31,3 @@ def new_user():
         'msg': f'{new_user.name} has successfully created an account'
     }, 201
 
-@user_bp.route("/<user_id>", methods=['GET'])
-def get_budget(user_id):
-    current_user = User.query.get(user_id)
-    current_budget = current_user.budget
-
-    if current_budget is None:
-        abort(make_response({'msg': 'budget is not set yet!'}, 404))
-    
-    return current_budget
-
-# @user_bp.route("/<user_id>", methods=['POST'])
-# def new_budget(user_id):
-#     request_body = request.get_json()
-    
-#     current_user = User.query.get(user_id)
-#     new_budget = Budget(amount=request_body['amount'], user = current_user)
-
-#     db.session.add(new_budget)
-#     db.session.commit()
-#     return {
-#         'id': new_budget.budget_id,
-#         'amount': new_budget.amount,
-#         'msg': f"A budget of ${new_budget.amount} has been set."
-#     }, 201
-
-@user_bp.route("/<user_id>", methods=['PATCH'])
-def edit_budget(user_id):
-    request_body = request.get_json()
-    
-    current_user = User.query.get(user_id)
-    current_user.budget = request_body['budget']
-
-    return {'msg': f'Budget ${current_user.budget} is added!'}, 200
