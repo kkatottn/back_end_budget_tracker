8279b1aa1c64d00bd004fa5315e4e669b9d8381c
Kelly Kang <shkx281@gmail.com>
2022-07-27 15:11:29 -0700

    Fixed relationship typo

--- app/models/category.py
+++ app/models/category.py
@@ -5,6 +5,6 @@ class Category(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=True)
-    user = db.Column("User", backpopulate='category')
-    expenses = db.relationship("Expense", back_populates="category", lazy=True)
+    user = db.Column("User", backpopulates='category')
+    expenses = db.relationship("Expense", backpopulates="category", lazy=True)
     
