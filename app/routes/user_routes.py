48ef89d1edf4f2b6d0861d0ce5f8339c9c195879
Kelly Kang <shkx281@gmail.com>
2022-08-03 11:22:45 -0700

    Removed id_token from user model, fixed user routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -15,16 +15,17 @@ def get_user(user_email):
     if not current_user:
         abort(make_response({'msg': 'user not found'}, 404))
     
-    return jsonify({"user_id": current_user.user_id, "id_token": current_user.id_token, "email": current_user.email, "name": current_user.name})    
+    return jsonify({"user_id": current_user.user_id, "email": current_user.email, "name": current_user.name})    
 
 @user_bp.route("", methods=['POST'])
 def new_user():
     request_body = request.get_json()
 
-    if "id_token" or "email" or "name" not in request_body:
+    # if "id_token" or "email" or "name" not in request_body:
+    if "email" or "name" not in request_body:
         return jsonify({"details": "missing part in request body"}), 400
     
-    new_user = User(id_token=request_body['id_token'], email=request_body['email'], name=request_body['name'])
+    new_user = User(email=request_body['email'], name=request_body['name'])
 
     db.session.add(new_user)
     db.session.commit()
